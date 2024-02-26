ba4ea1e326eb9ef235dee1c49b73c1699f75f8a3
Michael <michael.jhaywoods@gmail.com>
2024-02-26 22:22:21 +0000

    Initial commit.

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,23 +1,23 @@
-import { LIGHT_THEME } from './theme/themes/light-theme';
-import { DARK_THEME } from './theme/themes/dark-theme';
-import { useTheme } from './plugins/components-plugin';
+import { LIGHT_THEME } from '@/theme/themes/light-theme';
+import { DARK_THEME } from '@/theme/themes/dark-theme';
+import { useTheme } from '@/plugins/components-plugin';
 import type { MenuItem } from '@/components/menu/menu-item';
-import Sidebar from './components/sidebar/Sidebar.vue';
-import Checkbox from './components/checkbox/Checkbox.vue';
-import Tag from './components/tag/Tag.vue';
-import Code from './components/code/Code.vue';
-import Menu from './components/menu/Menu.vue';
-import Button from './components/button/Button.vue';
-import Layout from './components/layout/Layout.vue';
-import Header from './components/header/Header.vue';
-import Switch from './components/switch/Switch.vue';
-import Tabs from './components/tabs/Tabs.vue';
-import Tab from './components/tabs/Tab.vue';
-import Table from './components/table/Table.vue';
-import TextField from './components/textfield/TextField.vue';
-import Column from './components/table/Column.vue';
-import ComponentsPlugin from './plugins/components-plugin';
-import IconButton from "./components/iconbutton/IconButton.vue";
+import Sidebar from '@/components/sidebar/Sidebar.vue';
+import Checkbox from '@/components/checkbox/Checkbox.vue';
+import Tag from '@/components/tag/Tag.vue';
+import Code from '@/components/code/Code.vue';
+import Menu from '@/components/menu/Menu.vue';
+import Button from '@/components/button/Button.vue';
+import Layout from '@/components/layout/Layout.vue';
+import Header from '@/components/header/Header.vue';
+import Switch from '@/components/switch/Switch.vue';
+import Tabs from '@/components/tabs/Tabs.vue';
+import Tab from '@/components/tabs/Tab.vue';
+import Table from '@/components/table/Table.vue';
+import TextField from '@/components/textfield/TextField.vue';
+import Column from '@/components/table/Column.vue';
+import ComponentsPlugin from '@/plugins/components-plugin';
+import IconButton from '@/components/iconbutton/IconButton.vue';
 
 export type { MenuItem };
 
@@ -40,5 +40,5 @@ export {
   TextField,
   Column,
   ComponentsPlugin,
-    IconButton
+  IconButton
 };
