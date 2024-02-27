b1ba95c3625944f4a7c1267d6a99b8866543c62f
Michael <michael.jhaywoods@gmail.com>
2024-02-27 22:01:53 +0000

    Initial commit.

--- packages/docs/src/components/docs/usage/CodeBlock.vue
+++ packages/docs/src/components/docs/usage/CodeBlock.vue
@@ -14,21 +14,12 @@ defineProps({
 </script>
 
 <template>
-  <VCodeBlock :code="code" prismjs :lang="lang" theme="tomorrow" />
+  <div class="code-block">
+    <VCodeBlock :code="code" prismjs :lang="lang" theme="tomorrow" />
+  </div>
 </template>
 
 <style scoped lang="sass">
-
-.preview-title
-  font-weight: 600
-  line-height: 1.2
-  margin: 1.5rem 0 1rem
-  font-size: 1.25rem
-
-.preview-description
-  display: block
-  margin-block-start: 1em
-  margin-block-end: 1em
-  margin-inline-start: 0
-  margin-inline-end: 0
+.code-block
+  max-width: 900px
 </style>
--- packages/docs/src/components/views/GettingStartedView.vue
+++ packages/docs/src/components/views/GettingStartedView.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import CodeBlock from '@/components/docs/usage/CodeBlock.vue';
+
+const installCode = `# Using npm
+npm install @aurora-lib/vue
+
+# Using yarn
+yarn add @aurora-lib/vue
+
+# Using pnpm
+pnpm add @aurora-lib/vue
+`;
 </script>
 
 <template>
@@ -7,10 +17,13 @@ import CodeBlock from '@/components/docs/usage/CodeBlock.vue';
 
   <h2>Installation</h2>
 
-  <CodeBlock lang="html" code="npm i @aurora-lib/vue"></CodeBlock>
+  <CodeBlock lang="html" :code="installCode"></CodeBlock>
 
   <h2>Import</h2>
-  <CodeBlock lang="html" code="import blah from ''"></CodeBlock>
+  <CodeBlock
+    lang="html"
+    code="import { Button } from '@aurora-lib/vue'"
+  ></CodeBlock>
 </template>
 
 <style scoped lang="sass"></style>
